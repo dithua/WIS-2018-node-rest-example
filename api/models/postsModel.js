1e2d7572de595aeeb92d9aad7b225cd7d219f309
Giannis Meletakis <meletakis@gmail.com>
2018-03-28 00:01:41 +0300

    Associate usersModel in postsModel
    Also, fix required fields in postsModel

--- api/models/postsModel.js
+++ api/models/postsModel.js
@@ -2,15 +2,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 var PostSchema = new Schema({
   title: {
     type: String,
-    required: 'Kindly enter the title of the post'
+    required: [true, 'Please add a title for the post']
   },
   body: {
     type: String,
-    required: 'Kindly enter the body of the post'
+    required: [true, 'Post without a body is not a post']
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'Users'
   },
 });
 
